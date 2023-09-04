f40de03c95c1d53fa6034a805c7227cf4fc44848
Pieter Bouckaert <bouckaert.pieter@vdabcampus.be>
2023-09-04 15:35:06 +0200

    k

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let filterwoord = prompt("woord");
 let result = []
 result = artikels.filter( artikel => artikel.artikelnaam.includes(filterwoord) );
 
- result.forEach(artikel=> {console.log(artikel.artikelnaam)});
+result.forEach(artikel=> {console.log(artikel.artikelnaam)});
 
 let minprijs = prompt("min prijs");
 let maxprijs = prompt("max prijs");
@@ -15,27 +15,26 @@ let verkoop = [];
 
 if(minprijs<maxprijs){
  verkoop = result.filter(artikel => artikel.verkoopprijs  >= minprijs && artikel.verkoopprijs <= maxprijs);
- let  gemiddelde= 0
+ let som = 0;
  verkoop.forEach(artikel=>{
-    gemiddelde += artikel.verkoopprijs / verkoop.length
+    som += artikel.verkoopprijs;
  })
 
-
-
-console.log(verkoop);
-console.log(gemiddelde);
-}else{
+    console.log(verkoop);
+    console.log(som / verkoop.length);
+}
+else{
     console.log("foute ingave min prijs is hoger dan max prijs");
 };
 
 function tikartikels(){
-const artikels = [];
-let artikelnaam = prompt("artikel");
-while (artikelnaam !== "stop") {
-    artikels.push ({ artikelnaam: artikelnaam , aankooprijs: Number( prompt ("aankoopprijs" )) , verkoopprijs: Number(prompt("verkoopprijs"))});
-    artikelnaam = prompt("artikel");   
-}
-return artikels
+  const artikels = [];
+  let artikelnaam = prompt("artikel");
+  while (artikelnaam !== "stop") {
+      artikels.push ({ artikelnaam: artikelnaam , aankooprijs: Number( prompt ("aankoopprijs" )) , verkoopprijs: Number(prompt("verkoopprijs"))});
+      artikelnaam = prompt("artikel");   
+  }
+  return artikels
 };
 
 
